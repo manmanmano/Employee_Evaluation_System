ed7abf6ed24b6e36a08f21f1cd6f3606b774888e
Jan Markus Üprus <juprus@ttu.ee>
2021-03-31 20:29:36 +0000

    Update employee.php

--- employee.php
+++ employee.php
@@ -31,11 +31,20 @@ session_start();
             </tr>
             <?php
             $grades = array(5 => 4.5, 6 => 3.9, 7 => 2.9);
-            foreach ($grades as $week => $grade) {
+            if (isset($_POST['search'])) {
+                $week = $_POST['search'];
                 echo "<tr>";
-                echo "<td>", $week, "</td>";
-                echo "<td><a href='grade popup'>", $grade, "</a></td>";
+                echo "<td>", $week, "<td>";
+                echo "<td>", $grades[$week], "</td>";
                 echo "</tr>";
+            }
+            } else {
+                foreach ($grades as $week => $grade) {
+                    echo "<tr>";
+                    echo "<td>", $week, "</td>";
+                    echo "<td><a href='grade popup'>", $grade, "</a></td>";
+                    echo "</tr>";
+                }
             }
             ?>
         </table>
