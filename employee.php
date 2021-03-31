2238201a54d7b1a3ade7142dfd57277041f41a5c
Jan Markus Üprus <juprus@ttu.ee>
2021-03-31 20:21:04 +0000

    Update employee.php

--- employee.php
+++ employee.php
@@ -22,8 +22,8 @@ session_start();
         <form>
             <label for="week">Week:</label>
             <input type="week" id="week" name="week">
-            <input type="submit" name="search" value="Search"><br>
-        </form>
+            <input type="submit" name="search" value="Search">
+        </form><br>
         <table>
             <tr>
                 <th>Week</th>
