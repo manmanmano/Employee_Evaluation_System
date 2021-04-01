173d7f1338f0f221bc728e6ba5fe1ae8d36c57f0
Andre Tomingas <antomi@ttu.ee>
2021-04-01 13:43:31 +0000

    Replace register.php

--- register/register.php
+++ register/register.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
@@ -15,56 +16,60 @@
 </head>
 <body>
 	<header>
-		<img class="logo" src="img/JAMLogo.png">
+		<p class="logo">Logo JAM</p>
 		<nav>
 			<a href="index.html">Main page</a>
 			<a href="#">Profile</a>
 		</nav>
 	</header>
-	<form action="registercomplete.html">
+	<form method="post" action="register1.php">
 		<label for="name"><b>Name</b></label>
-		<input type="text" id="name" placeholder="Full Name" required>
+		<input type="text" id="name" name="name"  placeholder="Full Name"  pattern="^[A-Za-z]+(\s[A-Za-z]+){0,2}$" required>
 		<br>
 		<br>
-		<label for="name"><b>Email</b></label>
-		<input type="text" id="email" placeholder="Email" required>
+		<label for="email"><b>Email</b></label>
+		<input type="text" id="email" name="email" placeholder="Email" required>
 		<br>
 		<br>
 		<label><b>Position:</b></label>
 			<ul>
 				<li>
 					<label>
-						<input type="radio" name="position" value="employee" id="employee" onclick="ShowHide()" checked>
+						<input type="radio" name="position" value="employee" id="employee" name="position" onclick="ShowHide()" checked>
 						<span>Employee</span>
 					</label>
 				</li>
 				<li>
 					<label>
-						<input type="radio" name="position" value="employer" id="employer" onclick="ShowHide()">
+						<input type="radio" name="position" value="employer" id="employer" name="position" onclick="ShowHide()">
 						<span>Employer</span>
 					</label>
 				</li>
 			</ul>
-		
+
+		<label for="name">Password must have at least 8 characters and at least one number, one uppercase letter and one lowercase letter.<br></label>
 		<label for="name"><b>Password</b></label>
-		<input type="password" id="pass" placeholder="Password" required>
+		<input type="password" id="password" placeholder="Password" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required>
 		<br>
 		<br>
 		<label for="name"><b>Confirm Password</b></label>
-		<input type="password" id="confPass" placeholder="Confirm Password" required>
+		<input type="password" id="cPassword" name="cPassword" placeholder="Confirm Password" required>
 		<br>
 		<br>
 		<div id="haveToken" style="display:block">
 			<label for="company"><b>Company token (token given by the employer)</b></label>
-			<input type="text" id="company" required>
+			<input type="text" id="company" name="company" required>
 		</div>
 
 		<div id="companyGen"  style="display:none">
-			<label for="genToken"><b>Company token (give this token to employees for registering)</b></label>
-			<input type="text" id="genToken">
+
+		<p><b><?php require_once("genToken.php");
+		echo "Give this token to employees for registering: ";
+		echo "<br>";
+		echo generateRandomString();?></b></p>
 		</div>
 		<br>
 		<button class="registerButton" type="submit"><b>Register</b></button>
-		
+	</form>
 </body>
 </html>
