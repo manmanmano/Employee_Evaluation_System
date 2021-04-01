5ba7dfc8655eb13d9e5b25d5d524d8c16b0f9372
Jan Markus Üprus <juprus@ttu.ee>
2021-04-01 12:29:03 +0000

    Update employee.php

--- employee.php
+++ employee.php
@@ -33,16 +33,20 @@ session_start();
             $grades = array(5 => 4.5, 6 => 3.9, 7 => 2.9);
             if (isset($_POST['search']) && intval(substr($_POST['week'], 6, 2)) != 0) {
                 $week = intval(substr($_POST['week'], 6, 2));
-                echo "<tr>";
-                echo "<td>", $week, "<td>";
-                echo "<td><a href='grade popup'>", $grades[$week], "</a></td>";
-                echo "</tr>";
+                ?>
+                <tr>
+                    <td><?php $week ?><td>
+                    <td><a href='grade popup'><?php $grades[$week] ?></a></td>
+                </tr>
+                <?php
             } else {
                 foreach ($grades as $week => $grade) {
-                    echo "<tr>";
-                    echo "<td>", $week, "</td>";
-                    echo "<td><a href='grade popup'>", $grade, "</a></td>";
-                    echo "</tr>";
+                    ?>
+                    <tr>
+                        <td><?php $week ?></td>
+                        <td><a href='grade popup'><?php $grade ?></a></td>
+                    </tr>
+                    <?php
                 }
             }
             ?>
