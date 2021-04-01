a591be5c711afc585d19f4665dbed662ca74e304
Mariano D'Angelo <madang@ttu.ee>
2021-04-01 15:53:43 +0300

    fixed empty bugs

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 if (isset($_POST['submit'])) {
 
     $title = $_POST['title'];
-    if (!isset($title)) {
+    if (!isset($title) || empty($title)) {
         die("Radio button left blank!");
     } else {
         if ($title != "employee" && $title != "employer") {
@@ -10,11 +10,11 @@ if (isset($_POST['submit'])) {
         }
     }
 
-    if (!filter_var($_POST['username'], FILTER_VALIDATE_EMAIL)) {
+    if (!filter_var($_POST['username'], FILTER_VALIDATE_EMAIL) || empty($_POST['username'])) {
         die("Invalid email in input!");
     }
 
-    if (strlen($_POST['password']) < 8) {
+    if (strlen($_POST['password']) < 8 || empty($_POST['password'])) {
         die("Invalid password in input. Too short!");
     }
 
