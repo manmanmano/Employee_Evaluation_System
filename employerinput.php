d59ed9493336da5760e3e6e94aa06727ce596042
Mariano D'Angelo <madang@ttu.ee>
2021-04-01 15:02:34 +0300

    added employerinput from Mariano

--- employerinput.php
+++ employerinput.php
@@ -1,3 +1,124 @@
+<?php
+if (isset($_POST['submit'])) {
+
+    $names = $_POST['worker_name'];
+    if (isset($names) && ($names != "james" || $names != "mary" || $names != "john")) {
+        die("Invalid worker name set!");
+    }
+
+
+
+    $initiative = $_POST['initiative'];
+    if (!isset($initiative)) {                                                       
+        die("Radio button left blank!");                                        
+    } else {                                                                    
+        if ($initiative != 1 && $initiative != 5) {                     
+            die("Corrupted data in radio input!");                              
+        }                                                                       
+    }
+    
+    $gbProjects = $_POST['group_based_projects'];
+    if (!isset($gbProjects)) {                                                      
+        die("Radio button left blank!");                                        
+    } else {                                                                    
+        if ($gbProjects != 1 && $gbProjects != 5) {                                
+            die("Corrupted data in radio input!");                                 
+        }                                                                          
+    }
+
+    $follows = $_POST['follows_instructions'];
+    if (!isset($follows)) {
+        die("Radio button left blank!");
+    } else {
+        if ($follows != 1 && $follows != 5) {
+            die("Corrupted data in radio input!");
+        }
+    }
+
+    $leadership = $_POST['leadership'];
+    if (!isset($leadership)) {
+        die("Radio button left blank!");
+    } else {
+        if ($leadership != 1 && $leadership != 5) {
+            die("Corrupted data in radio input!");
+        }
+    }
+
+    $focused = $_POST['focused'];
+    if (!isset($focused)) {
+        die("Radio button left blank!");
+    } else {
+        if ($focused != 1 && $focused != 5) {
+            die("Corrupted data in radio input!");
+        }
+    }
+
+    $prioritize = $_POST['prioritize'];
+    if (!isset($prioritize)) {
+        die("Radio button left blank!");
+    } else {
+        if ($prioritize != 1 && $prioritize != 5) {
+            die("Corrupted data in radio input!");
+        }
+    }
+
+    $workers = $_POST['communication_coworkers'];
+    if (!isset($workers)) {
+        die("Radio button left blank!");
+    } else {
+        if ($workers != 1 && $workers != 5) {
+            die("Corrupted data in radio input!");
+        }
+    }
+
+    $superiors = $_POST['communication_superiors'];
+    if (!isset($superiors)) {
+        die("Radio button left blank!");
+    } else {
+        if ($superiors != 1 && $superiors != 5) {
+            die("Corrupted data in radio input!");
+        }
+    }
+
+    $dependable = $_POST['dependable'];
+    if (!isset($dependable)) {
+        die("Radio button left blank!");
+    } else {
+        if ($dependable != 1 && $dependable != 5) {
+            die("Corrupted data in radio input!");
+        }
+    }
+
+    $punctualAss = $_POST['assignments_on_time'];
+    if (!isset($punctualAss)) {
+        die("Radio button left blank!");
+    } else {
+        if ($punctualAss != 1 && $punctualAss != 5) {
+            die("Corrupted data in radio input!");
+        }
+    }
+
+    $punctualTime = $_POST['arrives_on_time'];
+    if (!isset($punctualTime)) {
+        die("Radio button left blank!");
+    } else {
+        if ($punctualTime != 1 && $punctualTime != 5) {
+            die("Corrupted data in radio input!");
+        }
+    }
+
+    $quality = $_POST['quality'];
+    if (!isset($quality)) {
+        die("Radio button left blank!");
+    } else {
+        if ($quality != 1 && $quality != 5) {
+            die("Corrupted data in radio input!");
+        }
+    }
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -9,23 +130,22 @@
         <header>
             <img class="logo" src="img/JAMLogo.png">
             <nav>
-                <a href="employer.html">Back to user page</a>
+                <a href="employer.php">Back to user page</a>
             </nav>
         </header>
+        <form action="employer.php" method="POST" name="updateEval">
         <table>
             <tr>
                 Name:
                 <select name="worker_name" required>
-                    <option>John Smith</option>
-                    <option>Mary Jane</option>
-                    <option>James Doe</option>
+                    <option value="john">John Smith</option>
+                    <option value="mary">Mary Jane</option>
+                    <option value="james">James Doe</option>
                 </select>
                 Week:
-                <select name="week" required>
-                    <option>22.02.21-28.02.21</option>
-                    <option>01.03.21-07.03.21</option>
-                    <option>08.03.21-14.03.21</option>
-                </select>
+                <input type="date" name="date" value"<?php echo date("Y-m-d"); ?>"
+                    max="<?php echo date('Y-m-d');?>" 
+                    min="<?php echo date('Y-m-d', strtotime($date . ' - 3 weeks')) ?>">
             </tr>
             <tr>
                 <th>Performance</th>
@@ -132,9 +252,10 @@
                 <td><input type="radio" name="quality" value="5"></td>
             </tr>
             <tr>
-                <td><input type="submit"></td>
+                <td><input type="submit" name="submit"></td>
             </tr>
         </table>
+        </form>
         <footer>
             <h3>Contact JAM</h3>
             <p>
