6b3b6c1b3a35c47c4ae8429da0d6a768e38396d4
Jan Markus Üprus <juprus@ttu.ee>
2021-04-05 11:07:40 +0000

    Update employee.php

--- employee/employee.php
+++ employee/employee.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require_once("employeetable.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,21 +30,7 @@ session_start();
                 <th>Evaluation</th>
             </tr>
             <?php
-            $grades = array(5 => 4.5, 6 => 3.9, 7 => 2.9);
-            if (isset($_POST['search']) && intval(substr($_POST['week'], 6, 2)) != 0) {
-                $week = intval(substr($_POST['week'], 6, 2));
-                echo "<tr>";
-                echo "<td>", $week, "<td>";
-                echo "<td><a href='grade popup'>", $grades[$week], "</a></td>";
-                echo "</tr>";
-            } else {
-                foreach ($grades as $week => $grade) {
-                    echo "<tr>";
-                    echo "<td>", $week, "</td>";
-                    echo "<td><a href='grade popup'>", $grade, "</a></td>";
-                    echo "</tr>";
-                }
-            }
+            createTable();
             ?>
         </table>
         <footer>
