0f52021fc0faf4ebd043d98aa6388c2d0a292d4b
Jan Markus Üprus <juprus@ttu.ee>
2021-04-05 11:06:33 +0000

    Update employer.php

--- employer/employer.php
+++ employer/employer.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require_once("employertable.php");
 ?>
 
 <!DOCTYPE html>
@@ -41,50 +42,7 @@ session_start();
                 <th>Evaluation</th>
             </tr>
             <?php
-            $employees = array(
-                'John Smith' => array(5 => 4.5, 6 => 3.9, 7 => 2.9),
-                'Mary Jane' => array(5 => 3.7, 6 => 4.3, 7 => 4.4),
-                'James Doe' => array(5 => 2.5, 6 => 3.5, 7 => 4.9),
-            );
-            if (isset($_POST['search']) && intval(substr($_POST['week'], 6, 2)) != 0 && empty($_POST['name'])) {
-                $week = intval(substr($_POST['week'], 6, 2));
-                foreach ($employees as $name => $grades) {
-                    if (!empty($grades[$week])) {
-                        echo "<tr>";
-                        echo "<td>", $name, "</td>";
-                        echo "<td>", $week, "</td>";
-                        echo "<td><a href='grade popup'>", $grades[$week], "</a></td>";
-                        echo "</tr>";
-                    }
-                }
-            } elseif (isset($_POST['search']) && intval(substr($_POST['week'], 6, 2)) == 0 && !empty($_POST['name'])) {
-                $name = $_POST['name'];
-                foreach ($employees[$name] as $week => $grade) {
-                    echo "<tr>";
-                    echo "<td>", $name, "</td>";
-                    echo "<td>", $week, "</td>";
-                    echo "<td><a href='grade popup'>", $grade, "</a></td>";
-                    echo "</tr>";
-                }
-            } elseif (isset($_POST['search']) && intval(substr($_POST['week'], 6, 2)) != 0 && !empty($_POST['name'])) {
-                $name = $_POST['name'];
-                $week = intval(substr($_POST['week'], 6, 2));
-                echo "<tr>";
-                echo "<td>", $name, "</td>";
-                echo "<td>", $week, "</td>";
-                echo "<td><a href='grade popup'>", $employees[$name][$week], "</a></td>";
-                echo "</tr>";
-            } else {
-                foreach ($employees as $name => $grades) {
-                    foreach ($grades as $week => $grade) {
-                        echo "<tr>";
-                        echo "<td>", $name, "</td>";
-                        echo "<td>", $week, "</td>";
-                        echo "<td><a href='grade popup'>", $grade, "</a></td>";
-                        echo "</tr>";
-                    }
-                }
-            }
+            createTable();
             ?>
         </table>
         <footer>
