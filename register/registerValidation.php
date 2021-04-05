9b6c7add496123ae08067c32bfa05539863b3d14
Andre Tomingas <antomi@ttu.ee>
2021-04-05 12:48:42 +0000

    now writes properly to users.csv

--- register/registerValidation.php
+++ register/registerValidation.php
@@ -20,84 +20,80 @@ function matching_passwords($password, $cpassword)
 
 
 
-if (!file_exists('users')) {
-    mkdir('usersData');
+
+
+$user = '';
+
+$name = $_POST['name'];
+if (empty($_POST['name'])){
+    exit("Please enter Your name.");
 }
-  fopen('../usersData/users.csv');
-
-  $user = '';
-
-  $name = $_POST['name'];
-  if (empty($_POST['name'])){
-      exit("Please enter Your name.");
-  }
-  // /^[a-zA-Z ]*$/ letters and whitespace or ^[A-Za-z]+(\s[A-Za-z]+){0,2}$ Only First, First & Last(?), First, Middle and Last
-  if(!preg_match("/^[a-zA-Z ]*$/", $name)){
-      exit("Please enter full name and use Latin alphabet.");
-      }
-
-  $user .= $name;
-  $user .= '; ';
-
-  $email = $_POST['email'];
-  if (empty($_POST['email'])){
-      exit("Please enter Your email.");
-  }
-  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      exit("Please enter a valid email.");
-  }
-  $user .= $email;
-  $user .= '; ';
-
-  /*
-
-  $position = $_POST['position'];
-  if($position == "employee"){
-
-  }
-  if($position == "employer"){
-
-  }
-  */
-
-
-  $position = $_POST['position'];
-  if(!isset($position) || empty($position)){
-    exit("Position must be selected.");
-  }else{
-    if($position != "employee" && $position != "employer"){
-      exit("Position must be 'Employee' or 'Employer'");
+// /^[a-zA-Z ]*$/ letters and whitespace or ^[A-Za-z]+(\s[A-Za-z]+){0,2}$ Only First, First & Last(?), First, Middle and Last
+if(!preg_match("/^[a-zA-Z ]*$/", $name)){
+    exit("Please enter full name and use Latin alphabet.");
     }
+
+$user .= $name;
+$user .= '; ';
+
+$email = $_POST['email'];
+if (empty($_POST['email'])){
+    exit("Please enter Your email.");
+}
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    exit("Please enter a valid email.");
+}
+$user .= $email;
+$user .= '; ';
+
+/*
+
+$position = $_POST['position'];
+if($position == "employee"){
+
+}
+if($position == "employer"){
+
+}
+*/
+
+
+$position = $_POST['position'];
+if(!isset($position) || empty($position)){
+  exit("Position must be selected.");
+}else{
+  if($position != "employee" && $position != "employer"){
+    exit("Position must be 'Employee' or 'Employer'");
   }
+}
 
-  $employeeToken = $_POST['company'];
-  if($position == "employee" && empty($employeeToken)){
-    exit("Token is required for registering");
-  }
+$employeeToken = $_POST['company'];
+if($position == "employee" && empty($employeeToken)){
+  exit("Token is required for registering");
+}
 
-  $password = $_POST['password'];
-  if (empty($_POST['password'])){
-      exit("Please choose a password.");
-  }
-  if(!preg_match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/', $password)){
-      exit("Password must have at least 8 characters and at least one number, one uppercase letter and one lowercase letter.");
-      }
+$password = $_POST['password'];
+if (empty($_POST['password'])){
+    exit("Please choose a password.");
+}
+if(!preg_match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/', $password)){
+    exit("Password must have at least 8 characters and at least one number, one uppercase letter and one lowercase letter.");
+    }
 
-  $cpassword = $_POST['cPassword'];
+$cpassword = $_POST['cPassword'];
 
-  matching_passwords($password, $cpassword);
+matching_passwords($password, $cpassword);
 
 
-  $user .= $password;
-  $user .= '; ';
-  $user .= $cpassword;
-  $user .= '; ';
+$user .= $password;
+$user .= '; ';
+$user .= $cpassword;
+$user .= '; ';
 
-	$user .= "\r\n";
+$user .= "\r\n";
 
-	$return = file_put_contents('usersData/users.csv', $user, FILE_APPEND);
+$return = file_put_contents('../usersData/users.csv', $user, FILE_APPEND);
 
-	fclose('usersData/users.csv');
 
-  header("refresh:0;registerComplete.php");
+header("refresh:0;registerComplete.php");
 ?>
