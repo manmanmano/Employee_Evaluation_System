2999881fa36e072a2f35777e6d07caf570e89275
Jan Markus Üprus <juprus@ttu.ee>
2021-04-06 03:51:02 +0000

    Update employeetable.php

--- employee/employeetable.php
+++ employee/employeetable.php
@@ -2,7 +2,7 @@
 function createTable() {
     $grades = array(05 => 4.5, 6 => 3.9, 7 => 2.9);
     $week = date("W", strtotime($_POST['date']));
-    if (isset($_POST['search']) && isset($_POST['date'])) {
+    if (isset($_POST['search']) && !empty($_POST['date'])) {
         echo "<tr>";
         echo "<td>", $week, "<td>";
         echo "<td><a href='grade popup'>", $grades[$week], "</a></td>";
