1353a0fdbec88339eade2b832e24792d34b95f85
Jan Markus Üprus <juprus@ttu.ee>
2021-04-06 03:44:08 +0000

    Update employeetable.php

--- employee/employeetable.php
+++ employee/employeetable.php
@@ -1,8 +1,7 @@
 <?php
 function createTable() {
     $grades = array(05 => 4.5, 06 => 3.9, 07 => 2.9);
-    $date = new DateTime($_POST['date']);
-    $week = $date->format("W");
+    $week = date("W", $_POST['date']);
     if (isset($_POST['search']) && isset($_POST['date'])) {
         echo "<tr>";
         echo "<td>", $week, "<td>";
