6b472aa2c2beef19384a6fdeae5f540081cffa1b
Andre Tomingas <antomi@ttu.ee>
2021-04-06 09:50:40 +0000

    edit

--- register/tokenExists.php
+++ register/tokenExists.php
@@ -1,15 +1,14 @@
-<?php 
+<?php
 function checkCsv($handle, $token) {
     $csvTokens = array();
-    $csvLine = explode(PHP_EOL, file_get_contents('users.csv'));
+    $csvLine = explode(PHP_EOL, file_get_contents('../usersData/users.csv'));
     foreach ($csvLine as $line) {
-        $values = explode(',', $line);
+        $values = explode(';', $line);
         array_push($csvTokens, $values[4]);
+
     }
-    if (in_array($token, $csvTokens)) {
-        echo "Token exists\n";
-    } else {
-        "Try again boomer\n";
-    }
-}
+    if (!in_array($token, $csvTokens)) {
+        exit ("Token doesn't exist");
+  }
+
 ?>
