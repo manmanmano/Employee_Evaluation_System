764f98f58eb69cebef6cf9ac64a55f94488b47ee
Jan Markus Üprus <juprus@ttu.ee>
2021-04-06 08:25:58 +0000

    Update employeetable.php

--- employee/employeetable.php
+++ employee/employeetable.php
@@ -3,9 +3,9 @@ function createTable() {
     $grades = array(5 => 4.5, 6 => 3.9, 7 => 2.9);
     $week = intval(date("W", strtotime($_POST['date'])));
     if (isset($_POST['search']) && !empty($_POST['date'])) {
-        $month = intval(date("M", strtotime($_POST['date'])));
-        $day = intval(date("D", strtotime($_POST['date'])));
-        $year = intval(date("Y", strtotime($_POST['date'])));
+        $month = intval(date("m", strtotime($_POST['date'])));
+        $day = intval(date("d", strtotime($_POST['date'])));
+        $year = intval(date("y", strtotime($_POST['date'])));
         if (!checkdate($month, $day, $year)) {                                                 
             die("Invalid date set!");
         }
