52bd03026c077306a3ee360512eda73d423bfd95
Jan Markus Üprus <juprus@ttu.ee>
2021-04-06 08:18:47 +0000

    Update employertable.php

--- employer/employertable.php
+++ employer/employertable.php
@@ -6,25 +6,20 @@ function createNames() {
     }
 }
 
-function validateDate($date) {
-    echo $date;
-    $month = intval(date("M", strtotime($date)));
-    $day = intval(date("D", strtotime($date)));
-    $year = intval(date("Y", strtotime($date)));
-    if (!checkdate($month, $day, $year)) {                                                 
-        die("Invalid date set!");
-    }
-}
-
 function createTable() {
     $employees = array(
         'John Smith' => array(5 => 4.5, 6 => 3.9, 7 => 2.9),
         'Mary Jane' => array(5 => 3.7, 6 => 4.3, 7 => 4.4),
         'James Doe' => array(5 => 2.5, 6 => 3.5, 7 => 4.9),
     );
+    $week = intval(date("W", strtotime($_POST['date'])));
     if (isset($_POST['search']) && !empty($_POST['date']) && empty($_POST['name'])) {
-        validateDate($_POST['date']);
-        $week = intval(date("W", strtotime($_POST['date'])));
+        $month = intval(date("M", strtotime($_POST['date'])));
+        $day = intval(date("D", strtotime($_POST['date'])));
+        $year = intval(date("Y", strtotime($_POST['date'])));
+        if (!checkdate($month, $day, $year)) {                                                 
+            die("Invalid date set!");
+        }
         foreach ($employees as $name => $grades) {
             if (!empty($grades[$week])) {
                 echo "<tr>";
@@ -44,9 +39,12 @@ function createTable() {
             echo "</tr>";
         }
     } elseif (isset($_POST['search']) && !empty($_POST['date']) && !empty($_POST['name'])) {
-        $name = $_POST['name'];
-        validateDate($_POST['date']);
-        $week = intval(date("W", strtotime($_POST['date'])));
+        $month = intval(date("M", strtotime($_POST['date'])));
+        $day = intval(date("D", strtotime($_POST['date'])));
+        $year = intval(date("Y", strtotime($_POST['date'])));
+        if (!checkdate($month, $day, $year)) {                                                 
+            die("Invalid date set!");
+        }
         echo "<tr>";
         echo "<td>", $name, "</td>";
         echo "<td>", $week, "</td>";
