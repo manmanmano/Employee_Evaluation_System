65db74315fb03c8c9b20638a7be5707979e232b3
Jan Markus Üprus <juprus@ttu.ee>
2021-04-06 08:59:09 +0000

    Update employerInput.php

--- employer/employerInput.php
+++ employer/employerInput.php
@@ -18,7 +18,7 @@ require_once("employerInputValidation.php");
                 <a href="employer.php">Back to user page</a>
             </nav>
         </header>
-        <form action="employerInputvalidation.php" method="POST" name="updateEval">
+        <form action="employerInputValidation.php" method="POST" name="updateEval">
         <table>
             <tr>
                 Name:
