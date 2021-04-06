83fc641d862e56b0ee5fd06a6b08782ea8ee8efe
Jan Markus Üprus <juprus@ttu.ee>
2021-04-06 04:04:01 +0000

    Update employerInput.php

--- employer/employerInput.php
+++ employer/employerInput.php
@@ -1,7 +1,8 @@
 <?php
 session_set_cookie_params(['path' => '/~juprus/icd0007_project/']);
 session_start();
-require_once("employerInputValidation.php")
+require_once("employerInputValidation.php");
+require_once("employertable.php");
 ?>
 
 <!DOCTYPE html>
