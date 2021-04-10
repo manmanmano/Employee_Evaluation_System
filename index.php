3d989313e21f6b5b71bfda35330d9ec966b8155c
Jan Markus Üprus <juprus@ttu.ee>
2021-04-10 17:43:07 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_set_cookie_params(['path' => '~/juprus/icd0007_project/']);                                                                
+session_start();
+?>
 
 <!DOCTYPE html>
 <html lang="en">
