c1a9ce522c2dafb2c295777ac0e3b53fdd8f21e5
Mariano D'Angelo <madang@ttu.ee>
2021-04-10 12:18:22 +0300

    cleaned up indexValidation

--- indexValidation/indexValidator.php
+++ indexValidation/indexValidator.php
@@ -1,31 +1,33 @@
 <?php                                                                           
 #if (isset($_POST['submit'])) {                                                  
                                                                                 
-    $title = $_POST['title'];                                                   
-    if (!isset($title) || empty($title)) {                                      
-        die("Radio button left blank!");                                        
-    } else {                                                                    
-        if ($title != "employee" && $title != "employer") {                     
-            die("Corrupted data in radio input!");                              
-        }                                                                       
-    }                                                                           
+$title = $_POST['title'];                                                   
+if (!isset($title) || empty($title)) {                                      
+    die("Radio button left blank!");                                        
+} else {                                                                    
+    if ($title != "employee" && $title != "employer") {                     
+        die("Corrupted data in radio input!");                              
+    }                                                                       
+}                                                                           
                                                                                 
-    $username = $_POST['email'];
-    if (!filter_var($username, FILTER_VALIDATE_EMAIL) || empty($username)) {
-        die("Invalid email in input!");                                         
-    }                                                                           
+$username = $_POST['email'];
+if (!filter_var($username, FILTER_VALIDATE_EMAIL) || empty($username)) {
+    die("Invalid email in input!");                                         
+}                                                                           
     
-    $password = $_POST['password'];    
-    if (strlen($password) < 8 || empty($password)) {          
-        die("Invalid password in input!");                           
-    }                                                                           
+$password = $_POST['password'];    
+if (strlen($password) < 8 || empty($password)) {          
+    die("Invalid password in input!");                           
+}                                                                           
     
-    if ($title == "employer") {
-        header("refresh:0; ../employer/employer.php");
-    } elseif ($title == "employee") {
-        header("refresh:0; ../employee/employee.php");
-    } else {
-        exit("Error!");
-    }
+if ($title == "employer") {
+    header("refresh:0; ../employer/employer.php");
+} elseif ($title == "employee") {
+    header("refresh:0; ../employee/employee.php");
+} else {
+    exit("Error!");
+}
+
+
 #}                                                                               
 ?> 
