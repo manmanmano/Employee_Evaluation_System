ff022c122bb76d61b7efd4b6b8d681838680aca0
Jan Markus Üprus <juprus@ttu.ee>
2021-04-10 11:06:13 +0000

    Update employertable.php

--- employer/employertable.php
+++ employer/employertable.php
@@ -1,4 +1,7 @@
 <?php
+session_set_cookie_params(['path' => '~/juprus/icd0007_project/']);                                                                
+session_start();
+
 if ($_SESSION['title'] != 'employer') {
     die("Incorrect credentials");
 }
