8ad9b289fc8f8482c125127a6adc1f992c5084ad
Jan Markus Üprus <juprus@ttu.ee>
2021-04-11 10:07:17 +0000

    Update indexValidator.php

--- indexValidation/indexValidator.php
+++ indexValidation/indexValidator.php
@@ -1,4 +1,5 @@
-<?php           
+<?php
+session_name("Evaluation");           
 session_set_cookie_params(['path' => '~/juprus/icd0007_project/']);                                                              
 session_start();                                                                           
 
