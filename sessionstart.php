2ace91075860f7a48c517c6374dacd761b909ddd
Mariano <madang@taltech.ee>
2021-04-12 20:57:41 +0300

    fixed document expiration

--- sessionstart.php
+++ sessionstart.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('session.cache_limiter', 'public');
+session_cache_limiter(false);
 session_name("Evaluation");
 session_set_cookie_params(['path' => '/~juprus/icd0007_project/']);                                                              
 session_start();
