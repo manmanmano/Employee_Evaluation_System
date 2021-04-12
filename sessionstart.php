199e7bf5ef824159d8180bb4fa1fdafa952b2e48
Mariano <madang@taltech.ee>
2021-04-12 21:02:18 +0300

    fixed path

--- sessionstart.php
+++ sessionstart.php
@@ -2,6 +2,6 @@
 ini_set('session.cache_limiter', 'public');
 session_cache_limiter(false);
 session_name("Evaluation");
-session_set_cookie_params(['path' => '/~juprus/icd0007_project/']);                                                              
+session_set_cookie_params(['path' => '/~madang/Web_Technologies/icd0007_project/');                                                              
 session_start();
 ?>
