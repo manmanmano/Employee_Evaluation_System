957a66209c957d5e2f824644c0d54c95e703df83
Jan Markus Üprus <juprus@ttu.ee>
2021-04-12 10:31:56 +0000

    Add new file

---
+++ sessionstart.php
@@ -0,0 +1,5 @@
+<?php
+session_name("Evaluation");
+session_set_cookie_params(['path' => '~/juprus/icd0007_project/']);                                                              
+session_start();
+?>
