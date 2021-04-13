1894b9cecbf4ed37d19e0a1c6111770996ad9bed
Mariano <madang@taltech.ee>
2021-04-13 11:42:33 +0300

    fixed login logout bug employee

--- employee/employee.php
+++ employee/employee.php
@@ -13,13 +13,13 @@ require_once("employeetable.php");
 		<header>
             <img class="logo" src="../img/JAMLogo.png">
 			<nav>
-				<form method="GET" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
+				<form method="POST" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
                     <input type="submit" name="logout" value="Log out">
                 </form>
 			</nav>
 		</header>
 		<h1>My evaluation</h1>
-        <form method="POST" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
+        <form method="GET" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
             <label for="date">Week:</label>
             <input type="date" id="date" name="date">
             <input type="submit" name="search" value="Search">
