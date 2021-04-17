1ef59ae90d1fec4992ab029e478bfea9024c36e0
Mariano D'Angelo <madang@ttu.ee>
2021-04-17 14:17:14 +0300

    implementation of addUser in progress. bound paramters

--- register/addUser.php
+++ register/addUser.php
@@ -4,6 +4,19 @@ function addUser($link, $position, $name, $email, $hashedPassword, $employeeToke
     //insert a new user
     $query = "INSERT INTO users (title, name, email, password, token)
         VALUES (?, ?, ?, ?, ?)";
+    if ($stmt = mysqli_prepare($link, $query)) {
+        //before binding check the position of the person
+        if ($position == "employer") {
+            //if employer than then the employee token
+            mysqli_stmt_bind_param($stmt, "sssss", 
+                $position, $name, $email, $hashedPassword, $employerToken);
+        } else {
+            //if not employer than employee
+            mysqli_stmt_bind_param($stmt, "sssss", 
+                $position, $name, $email, $hashedPassword, $employeeToken);
+        } 
+        //if execute is successful redirect to registrationSuccess
+    }
 }
 
 ?>
