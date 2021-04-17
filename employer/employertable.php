5853d236dadab55e7eeef3e9ed411f1ae1fdabbf
Jan Markus Üprus <juprus@ttu.ee>
2021-04-17 11:51:18 +0000

    Update employertable.php

--- employer/employertable.php
+++ employer/employertable.php
@@ -15,9 +15,8 @@ function createNames($token) {
         die("Connection to DB failed: " . mysqli_connect_error());
     }
 
-    $sanitizedtoken = sanitizeInputVar($link, $token);
     $query = mysqli_prepare($link, "SELECT name FROM users WHERE token='?';");
-    mysqli_stmt_bind_param($query, "s", $sanitizedtoken);
+    mysqli_stmt_bind_param($query, "s", sanitizedInputVar($link, $token));
     mysqli_stmt_execute($query);
     mysqli_stmt_bind_result($query, $name);
     $names = array();
