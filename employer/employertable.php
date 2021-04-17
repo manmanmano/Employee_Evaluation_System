7ccdc384847bdecd5186e587d6f81d48e40f3b47
Jan Markus Üprus <juprus@ttu.ee>
2021-04-17 11:32:56 +0000

    Update employertable.php

--- employer/employertable.php
+++ employer/employertable.php
@@ -5,7 +5,7 @@ if ($_SESSION['title'] != 'employer') {
     die("Incorrect credentials");
 }
 
-function createNames() {
+function createNames($token) {
     include_once("../usersData/connect.db.php");
     include_once("../usersData/sanitizeInputVar");
 
@@ -16,7 +16,7 @@ function createNames() {
     }
 
     $query = mysqli_prepare($link, "SELECT name FROM users WHERE token=?;");
-    mysqli_stmt_bind_param($query, "s", sanitizeInputVar($link, $_SESSION['token']));
+    mysqli_stmt_bind_param($query, "s", sanitizeInputVar($link, $token));
     mysqli_stmt_execute($query);
     mysqli_stmt_bind_result($query, $name);
     $names = array();
