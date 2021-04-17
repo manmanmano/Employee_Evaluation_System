0d8a5d0e7f080fbb405aeaaedf53fb8fab5ccb97
Jan Markus Üprus <juprus@ttu.ee>
2021-04-17 16:16:06 +0000

    Update employeetable.php

--- employee/employeetable.php
+++ employee/employeetable.php
@@ -58,7 +58,7 @@ function createTable($token) {
                 echo "<tr>";
                 echo "<td>", $week, "</td>";
                 echo "<td>", $year, "</td>";
-                echo "<td><a class='btn btn-primary' data-toggle='modal' href='modal.html' data-target='#MyModal' data-backdrop='static'>", $grade,"</a></td>";
+                echo "<td><a class='btn btn-primary' data-toggle='modal' href='#MyModal' data-target='#MyModal' data-backdrop='static'>", $grade,"</a></td>";
                 echo "</tr>";
             }
         }
