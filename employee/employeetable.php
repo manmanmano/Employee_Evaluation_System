5549413fab98bf007bd0f2792aaf6d20776d5f4c
Jan Markus Üprus <juprus@ttu.ee>
2021-04-19 18:22:27 +0000

    Update employeetable.php

--- employee/employeetable.php
+++ employee/employeetable.php
@@ -78,7 +78,7 @@ function createTable($token) {
         echo "<tr>";
         echo "<td>", $week, "</td>";
         echo "<td>", $year, "</td>";
-        echo "<td><a href='/' onclick='modal()'>", $grades[$year][$week], "</a></td>";
+        echo "<td><a href='' onclick='modal()'>", $grades[$year][$week], "</a></td>";
         echo "</tr>";
     } else {
         foreach ($grades as $year => $weeks) {
@@ -86,7 +86,7 @@ function createTable($token) {
                 echo "<tr>";
                 echo "<td>", $week, "</td>";
                 echo "<td>", $year, "</td>";
-                echo "<td><a href='/' onclick='modal()'>", $grade, "</a></td>";
+                echo "<td><a href='' onclick='modal()'>", $grade, "</a></td>";
                 echo "</tr>";
             }
         }
