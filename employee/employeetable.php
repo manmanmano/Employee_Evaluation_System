9c6abfa873d1bd127e6204edbd997d706918c6c2
Jan Markus Üprus <juprus@ttu.ee>
2021-04-19 17:12:56 +0000

    Update employeetable.php

--- employee/employeetable.php
+++ employee/employeetable.php
@@ -16,8 +16,6 @@ if (isset($_GET['week']) && isset($_GET['year'])) {
     }
 
     $name = "'" . $_SESSION['name'] . "'";
-    echo $_GET['week'];
-    echo $_GET['year'];
     $query = mysqli_prepare($link, "SELECT * FROM token_? WHERE name=? AND week=? AND year=?;");
     mysqli_stmt_bind_param($query, "ssii", $_SESSION['token'], $name, $_GET['week'], $_GET['year']);
     mysqli_stmt_execute($query);
