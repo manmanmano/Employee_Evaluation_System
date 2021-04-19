a29a37c8632771c842d5a61a258a8168d943d4b6
Jan Markus Üprus <juprus@ttu.ee>
2021-04-19 12:02:41 +0000

    Update employee.php

--- employee/employee.php
+++ employee/employee.php
@@ -18,7 +18,6 @@ require_once("employeetable.php");
 			</nav>
 		</header>
         <h1>Welcome <?php echo $_SESSION['name']; ?>!</h1>
-		<h1>My evaluation</h1>
         <form method="GET" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
             <label for="date">Week:</label>
             <input type="date" id="date" name="date">
