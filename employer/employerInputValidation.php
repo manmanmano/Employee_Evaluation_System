e0cb91e5a461b71f16671005097f3ad203a04e05
Mariano D'Angelo <madang@ttu.ee>
2021-04-19 14:08:28 +0300

    validateRadio used to validate input

--- employer/employerInputValidation.php
+++ employer/employerInputValidation.php
@@ -2,10 +2,6 @@
 require_once("../sessionstart.php");
 include_once("../usersData/connect.db.php");
 
-if ($_SESSION['title'] != 'employer') {
-    die("Incorrect credentials");
-}
-
 function createNames($token, $link) {
     include_once("../usersData/sanitizeInputVar");
 
@@ -27,7 +23,7 @@ function createNames($token, $link) {
 
 function validateRadio($input) {
     if (!isset($input) || empty($input)) {                            
-        die("Radio button left blank!");                                        
+        die("Radio button left blank!");
     } else {                                                                    
         if ($input < 1 || $input > 5) {                               
             die("Corrupted data in radio input!");                              
@@ -37,6 +33,10 @@ function validateRadio($input) {
 
 function evaluateEmployee($arr) {
     return array_sum($arr) / count($arr);
+}
+
+if ($_SESSION['title'] != 'employer') {
+    die("Incorrect credentials");
 }
 
 $link = mysqli_connect($server, $user, $password, $database);
@@ -59,114 +59,41 @@ if (!empty($names) && !in_array($workerName, $names)) {
 }                                                                           
                                                                             
 $initiative = $_POST['initiative'];                                         
-if (!isset($initiative) || empty($initiative)) {                            
-    die("Radio button left blank!");                                        
-} else {                                                                    
-    if ($initiative < 1 || $initiative > 5) {                               
-        die("Corrupted data in radio input!");                              
-    }                                                                       
-} 
+validateRadio($initiative);
 
 $gbProjects = $_POST['group_based_projects'];                               
-if (!isset($gbProjects) || empty($gbProjects)) {                            
-    die("Radio button left blank!");                                        
-} else {                                                                    
-    if ($gbProjects < 1 || $gbProjects > 5) {                               
-        die("Corrupted data in radio input!");                              
-    }                                                                       
-}    
-
+validateRadio($gbProjects);
                                                                                 
 $follows = $_POST['follows_instructions'];                                  
-if (!isset($follows) || empty($follows)) {                                  
-    die("Radio button left blank!");                                        
-} else {                                                                    
-    if ($follows < 1 || $follows > 5) {                                     
-        die("Corrupted data in radio input!");                              
-    }                                                                       
-}                                                                           
-                                                                            
+validateRadio($follows);
+
 $leadership = $_POST['leadership'];                                         
-if (!isset($leadership) || empty($leadership)) {                            
-    die("Radio button left blank!");                                        
-} else {                                                                    
-    if ($leadership < 1 || $leadership > 5) {                               
-        die("Corrupted data in radio input!");                              
-    }                                                                       
-}                                                                           
-                                                                            
+validateRadio($leadership);                                                                            
+
 $focused = $_POST['focused'];                                               
-if (!isset($focused) || empty($focused)) {                                  
-    die("Radio button left blank!");                                        
-} else {                                                                    
-    if ($focused < 1 || $focused > 5) {                                     
-        die("Corrupted data in radio input!");                              
-    }                                                                       
-}                                                                           
-                                                                            
+validateRadio($focused);                                                                            
+
 $prioritize = $_POST['prioritize'];                                         
-if (!isset($prioritize) || empty($prioritize)) {                            
-    die("Radio button left blank!");                                        
-} else {                                                                    
-    if ($prioritize < 1 || $prioritize > 5) {                               
-        die("Corrupted data in radio input!");                              
-    }                                                                       
-}                                                                           
-                                                                            
+validateRadio($prioritize);                                                                            
+
 $workers = $_POST['communication_coworkers'];                               
-if (!isset($workers) || empty($workers)) {                                  
-    die("Radio button left blank!");                                        
-} else {                                                                    
-    if ($workers < 1 || $workers > 5) {                                     
-        die("Corrupted data in radio input!");                              
-    }                                                                       
-}                                                                           
-                                                                            
+validateRadio($workers);                                                                            
+
 $superiors = $_POST['communication_superiors'];                             
-if (!isset($superiors) || empty($superiors)) {                              
-    die("Radio button left blank!");                                        
-} else {                                                                    
-    if ($superiors < 1 || $superiors > 5) {                                 
-        die("Corrupted data in radio input!");                              
-    }                                                                       
-}                                                                           
-                                                                            
+validateRadio($superiors);
+
 $dependable = $_POST['dependable'];                                         
-if (!isset($dependable) || empty($dependable)) {                            
-    die("Radio button left blank!");                                        
-} else {                                                                    
-    if ($dependable < 1 || $dependable > 5) {                               
-        die("Corrupted data in radio input!");                              
-    }                                                                       
-}                                                                           
-                                                                            
- $punctualAss = $_POST['assignments_on_time'];                              
-if (!isset($punctualAss) || empty($punctualAss)) {                          
-    die("Radio button left blank!");                                        
-} else {                                                                    
-    if ($punctualAss < 1 || $punctualAss > 5) {                             
-        die("Corrupted data in radio input!");                              
-    }                                                                       
-}                                                                           
-                                                                            
+validateRadio($dependable);                                                                            
+
+$punctualAss = $_POST['assignments_on_time'];                              
+validateRadio($punctualAss);                                                                            
+
 $punctualTime = $_POST['arrives_on_time'];                                  
-if (!isset($punctualTime) || empty($punctualTime)) {                        
-    die("Radio button left blank!");                                        
-} else {                                                                    
-    if ($punctualTime < 1 || $punctualTime > 5) {                           
-        die("Corrupted data in radio input!");                              
-    }                                                                       
-}                                                                           
-                                                                            
+validateRadio($punctualTime);                                                                            
+
 $quality = $_POST['quality'];                                               
-if (!isset($quality) || empty($quality)) {                                  
-    die("Radio button left blank!");                                        
-} else {                                                                    
-    if ($quality < 1 || $quality > 5) {                                     
-        die("Corrupted data in radio input!");                              
-    }                                                                       
-}                                                                           
-                                                                            
+validateRadio($quality);
+
 $attrArr = [                                                                
     $initiatve, $gbProjects, $follows, $leadership, $focused, $prioritize,  
     $workers, $superiors, $dependable, $punctualAss, $punctualTime, $quality
