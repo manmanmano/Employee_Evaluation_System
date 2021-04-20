c6f2dbac7da2952b232e6517a94122f60f3fdd28
Mariano D'Angelo <madang@ttu.ee>
2021-04-20 15:16:40 +0300

    sanitize inputs works

--- employee/validateEmployeeCredentials.php
+++ employee/validateEmployeeCredentials.php
@@ -5,17 +5,10 @@ require_once("../usersData/connect.db.php");
 // checks if password and cPassword match
 function matching_passwords($password, $cpassword)
 {
-    // Your validation code.
-    if (empty($password)) {
-        exit("Password is required.");
-    }
-    else if ($password != $cpassword) {
+    if ($password != $cpassword) {
         // error matching passwords
         exit('Your passwords do not match.');
-
     }
-    // passwords match
-    return true;
 }
 
 function checkEmail($link, $email) {
@@ -31,16 +24,51 @@ function checkEmail($link, $email) {
             mysqli_stmt_store_result($stmt);
             //if email does not exist exit
             if (mysqli_stmt_num_rows($stmt) == 1) {
-                echo "<h1>";
-                die("Email already exists!");
-                echo "</h1>";
+                echo "<h1>Email already exists!</h1>";
+                exit();
             }
         }
         mysqli_stmt_close($stmt);
     }
 }
 
-//connect to database, in case of failure give error
-$link = mysqli_connect($server, $user, $password, $database);
-if (!$link) die("Connection to DB failed: " . mysqli_connect_error());
+function updateData($link, $email, $password) {
+
+}
+
+if (isset($_POST['newData'])) {                                                   
+    //connect to database, in case of failure give error
+    $link = mysqli_connect($server, $user, $password, $database);
+    if (!$link) die("Connection to DB failed: " . mysqli_connect_error());
+
+    $password = $_POST['newPassword'];
+    if (isset($password) && !empty($password)) {
+        if(!preg_match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/', $password)) {
+            exit("Invalid password");
+        }
+    }
+
+    $cpassword = $_POST['newcPassword'];
+    if (!empty($password) && isset($cpassword) && !empty($cpassword)) {
+        if(!preg_match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/', $cpassword)) {
+            exit("Invalid password");
+        }
+    }
+
+    matching_passwords($password, $cpassword);
+
+    $email = $_POST['newEmail'];
+    if (isset($email) && !empty($email)) {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            exit("Invalid email");
+        } else {
+            checkEmail($link, $email);
+        }
+    }
+
+    updateData($link, $password, $email);
+    mysqli_close($link);
+}
+
+
 ?>
