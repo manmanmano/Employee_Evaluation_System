d8a038a1659f9af05b4582ba59cb2665da3b78a1
Mariano D'Angelo <madang@ttu.ee>
2021-04-20 22:47:18 +0300

    remove company works

--- employer/removeItem.php
+++ employer/removeItem.php
@@ -5,7 +5,7 @@ require_once("../usersData/connect.db.php");
 $link = mysqli_connect($server, $user, $password, $database);
 if (!$link) die("Connection to DB failed: " . mysqli_connect_error());
 
-$query = "DROP TABLE token_? ;";
+$query = "DROP TABLE token_" . $_SESSION['token'] . ";";
 $stmt = mysqli_prepare($link, $query);
 mysqli_stmt_bind_param($stmt, "s", $_SESSION["token"]);
 mysqli_stmt_execute($stmt);
