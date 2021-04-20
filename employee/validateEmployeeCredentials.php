876856152bae40a6d52db8c62ce2c8905ed0866b
Mariano D'Angelo <madang@ttu.ee>
2021-04-20 13:16:08 +0300

    connection established and check email and password added

--- employee/validateEmployeeCredentials.php
+++ employee/validateEmployeeCredentials.php
@@ -1,3 +1,46 @@
 <?php 
 require_once("../sessionstart.php");
+require_once("../usersData/connect.db.php");
+
+// checks if password and cPassword match
+function matching_passwords($password, $cpassword)
+{
+    // Your validation code.
+    if (empty($password)) {
+        exit("Password is required.");
+    }
+    else if ($password != $cpassword) {
+        // error matching passwords
+        exit('Your passwords do not match.');
+
+    }
+    // passwords match
+    return true;
+}
+
+function checkEmail($link, $email) {
+        //prepare a select statement
+    $query = "SELECT email FROM users WHERE email=?";
+
+    if ($stmt = mysqli_prepare($link, $query)) {
+        //bind the email to the prepared statement
+        mysqli_stmt_bind_param($stmt, "s", $email);
+        //if execution is successful check for the email
+        if (mysqli_stmt_execute($stmt)) {
+            //store the result locally
+            mysqli_stmt_store_result($stmt);
+            //if email does not exist exit
+            if (mysqli_stmt_num_rows($stmt) == 1) {
+                echo "<h1>";
+                die("Email already exists!");
+                echo "</h1>";
+            }
+        }
+        mysqli_stmt_close($stmt);
+    }
+}
+
+//connect to database, in case of failure give error
+$link = mysqli_connect($server, $user, $password, $database);
+if (!$link) die("Connection to DB failed: " . mysqli_connect_error());
 ?>
