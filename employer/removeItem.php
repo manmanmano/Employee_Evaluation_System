cb838a602658af7e4b0113732325ef106540933e
Mariano D'Angelo <madang@ttu.ee>
2021-04-20 22:39:33 +0300

    delete company done. not tested

--- employer/removeItem.php
+++ employer/removeItem.php
@@ -1,3 +1,21 @@
 <?php
+require_once("../sessionstart.php");
+require_once("../usersData/connect.db.php");
 
+$link = mysqli_connect($server, $user, $password, $database);
+if (!$link) die("Connection to DB failed: " . mysqli_connect_error());
+
+$query = "DROP TABLE token_? ;";
+$stmt = mysqli_prepare($link, $query);
+mysqli_stmt_bind_param($stmt, "s", $_SESSION["token"]);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_close($stmt);
+
+$query = "DELETE FROM users WHERE token = ?";
+$stmt = mysqli_prepare($link, $query);
+mysqli_stmt_bind_param($stmt, "s", $_SESSION['token']);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_close($stmt);
+
+mysqli_close($link);
 ?>
