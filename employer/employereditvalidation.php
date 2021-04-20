aa36cfd1ca79a0129dc85547bb383ff8ab751553
Jan Markus Üprus <juprus@ttu.ee>
2021-04-20 11:05:16 +0000

    Update employereditvalidation.php

--- employer/employereditvalidation.php
+++ employer/employereditvalidation.php
@@ -6,21 +6,6 @@ if ($_SESSION['title'] != 'employer') {
     die("Session expired!");
 }
 
-function createNames($token, $link) {
-    include_once("../usersData/sanitizeInputVar");
-
-    if (!$link) {
-        die("Connection to DB failed: " . mysqli_connect_error());
-    }
-
-    $query = "SELECT name FROM users WHERE token='". $token . "' AND title='employee';";
-    $result = mysqli_query($link, $query);
-    $names = array();
-    while ($row = mysqli_fetch_assoc($result)) {
-        array_push($names, $row['name']);
-    }
-}
-
 function validateRadio($input) {
     if (!isset($input) || empty($input)) {                            
         die("Radio button left blank!");
@@ -35,17 +20,13 @@ function evaluateEmployee($arr) {
     return array_sum($arr) / count($arr);
 }
 
-function editEval($link, $token, $name, $week, $year, $average, $initiative, $gbProjects, 
-    $follows, $leadership, $focused, $prioritize, $workers, $superiors, $dependable, 
-    $punctualAss, $punctualTime, $quality) {
+function editEval($link, $token, $name, $week, $year, $average, $initiative, $gbProjects, $follows, $leadership, $focused, $prioritize, $workers, $superiors, $dependable, $punctualAss, $punctualTime, $quality) {
 
     $query = "UPDATE token_" . $token . " SET average=?, initiative=?, group_based_projects=?, follows_instructions=?, leadership=?, focused=?, prioritize=?, communication_coworkers=?, communication_superiors=?, dependable=?, assignments_on_time=?, arrives_on_time=?, quality=? WHERE name=? AND week=? AND year=?;";
 
     if ($stmt = mysqli_prepare($link, $query)) {
         //bind variables to parameters
-        mysqli_stmt_bind_param($stmt, "diiiiiiiiiiiisii", $average, $initiative, $gbProjects, $follows, $leadership, $focused, 
-            $prioritize, $workers, $superiors, $dependable, $punctualAss,
-            $punctualTime, $quality, $name, $week, $year);
+        mysqli_stmt_bind_param($stmt, "diiiiiiiiiiiisii", $average, $initiative, $gbProjects, $follows, $leadership, $focused, $prioritize, $workers, $superiors, $dependable, $punctualAss, $punctualTime, $quality, $name, $week, $year);
         //attempt to execute the statement
         if (mysqli_stmt_execute($stmt)) {
             header("refresh:0;employer.php");
@@ -59,7 +40,20 @@ function editEval($link, $token, $name, $week, $year, $average, $initiative, $gb
 $link = mysqli_connect($server, $user, $password, $database);
 if (!$link) die("Connection to DB failed: " . mysqli_connect_error());
 
-if (isset($_POST['submit'])) {                                                                        
+if (isset($_POST['submit'])) {     
+    include_once("../usersData/sanitizeInputVar");
+
+    if (!$link) {
+        die("Connection to DB failed: " . mysqli_connect_error());
+    }
+
+    $token = $_SESSION['token'];
+    $query = "SELECT name FROM users WHERE token='". $token . "' AND title='employee';";
+    $result = mysqli_query($link, $query);
+    $names = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        array_push($names, $row['name']);
+    }                                                                   
 
     $sanitizedweek = sanitizeInputVar($link, $week);
     $sanitizedyear = sanitizeInputVar($link, $year);
