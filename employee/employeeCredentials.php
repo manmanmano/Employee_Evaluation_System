0979c98393c6fdc4a54ecf17c5b80e3125cb7680
Mariano D'Angelo <madang@ttu.ee>
2021-04-20 11:56:01 +0300

    started user's credentials

---
+++ employee/employeeCredentials.php
@@ -0,0 +1,13 @@
+<?php?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta charset="UTF-8">
+    <title>Employee's credentials</title>
+    <link rel="stylesheet" href="../styles/style.css">
+</head>
+<body>
+    
+</body>
+</html>
