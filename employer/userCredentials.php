569d10ea18d713a3f447b8242e8b6f4146eeee8b
Andre Tomingas <antomi@ttu.ee>
2021-04-20 20:06:22 +0000

    added div to delete company button

--- employer/userCredentials.php
+++ employer/userCredentials.php
@@ -44,8 +44,10 @@ require_once("validateUserCredentials.php");
         <input type="submit" name="newData" value="Update your data">
     </form><br>
     <h1 style="color: red; text-align: center; padding:10px;">Delete Your company</h1>
+    <div class="deleteCompBtn">
     <input type="submit" name="delCompany" id="deleteCompany" value="Destroy company" 
         onclick="deleteCompany()"> 
+    </div>
     <footer id="footor" style=" margin-top: 200px; margin-bottom: 0px;">
         <h2>Contact JAM</h2>
         <p id="email">Send us an email to:<br><a href="mailto: fake@mail.com">info@JAM.com</a> </p>
