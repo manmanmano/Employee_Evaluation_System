7b18a066d233a14164b63fd9fc03994967e63629
Jan Markus Üprus <juprus@ttu.ee>
2021-04-20 13:41:50 +0000

    Update employerdelete.php

--- employer/employerdelete.php
+++ employer/employerdelete.php
@@ -13,8 +13,12 @@ if (!$link) die("Connection to DB failed: " . mysqli_connect_error());
 $name = sanitizeInputVar($link, $_GET['name']);
 $week = sanitizeInputVar($link, $_GET['week']);
 $year = sanitizeInputVar($link, $_GET['year']);
+echo $name;
+echo $week;
+echo $year;
 $query = "DELETE FROM token_" . $_SESSION['token'] . " WHERE name='" . $name . "' AND week=" . $week . " AND year=" . $year . ";";
 $result = mysqli_query($link, $query);
 
 mysqli_close($link);
+header("refresh:0;url=employer.php");
 ?>
