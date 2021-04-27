d21786246595b97fb4c37aefc374bb8f382ce887
Jan Markus Üprus <juprus@ttu.ee>
2021-04-27 12:48:33 +0000

    Update employerdelete.php

--- employer/employerdelete.php
+++ employer/employerdelete.php
@@ -1,21 +1,23 @@
 <?php
+//starting session, getting database parameters and sanitization function
 require_once("../sessionstart.php");
 include_once("../usersData/connect.db.php");
 include_once("../usersData/sanitizeInputVar.php");
 
+//checking user title
 if ($_SESSION['title'] != 'employer') {
     die("Session expired!");
 }
 
+//connecting to database
 $link = mysqli_connect($server, $user, $password, $database);
 if (!$link) die("Connection to DB failed: " . mysqli_connect_error());
 
+//getting variables to form query
 $name = sanitizeInputVar($link, $_GET['name']);
 $week = sanitizeInputVar($link, $_GET['week']);
 $year = sanitizeInputVar($link, $_GET['year']);
-echo $name;
-echo $week;
-echo $year;
+//deleting the evaluation from table
 $query = "DELETE FROM token_" . $_SESSION['token'] . " WHERE name='" . $name . "' AND week=" . $week . " AND year=" . $year . ";";
 $result = mysqli_query($link, $query);
 
