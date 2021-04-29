a382a0b5e8047f0dcc1bb7ce129e644ce5facf0a
Andre Tomingas <antomi@ttu.ee>
2021-04-29 15:56:48 +0000

    added comments

--- register/registerJS.js
+++ register/registerJS.js
@@ -1,10 +1,11 @@
+// hovering over "?" shows password as plain text 
 function ShowHide(){
 var employee = document.getElementById("employee");
 var employer = document.getElementById("employer");
 companyGen.style.display = employee.checked ? "none" : "block";
 haveToken.style.display = employee.checked ? "block" : "none";
 }
-
+// hovering over "?" shows password as plain text for "password"
 function showPwd(){
     document.getElementById("password").type='text';
   }
@@ -13,6 +14,7 @@ function hidePwd(){
     document.getElementById("password").type='password';
   }
 
+//hovering over "?" shows password as plain text for "confirm password"
 function showcPwd(){
     document.getElementById("cPassword").type='text';
   }
