9905d524710d4c7bb738a6a72725b31a6002ebb9
Mariano D'Angelo <madang@ttu.ee>
2021-04-30 03:08:09 +0300

    comment

--- scripts/deleteCompany.js
+++ scripts/deleteCompany.js
@@ -1,5 +1,7 @@
 function deleteCompany() {
+    //if the employer presses the destroy company button than he is prompted a confirm box
     if (confirm('Are you sure you want to destroy all of the data related to your company?')) {
+        //if the answer is yes than redirect and execute the script to destroy the company
         location.href = "../employer/removeItem.php";
     }
 }
